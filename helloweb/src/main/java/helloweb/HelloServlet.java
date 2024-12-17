3118f20ad223a3338b73c57b5c7df5322369b06c
minseokim0629 <han8121449@naver.com>
2024-12-17 13:25:15 +0900

    241217_1 practice HelloServlet

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,27 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8"); // 브라우저한테 헤더 마지막 부분을 보내주게 되고
+		PrintWriter out = response.getWriter(); // 빈 개행을 출력한다
+		out.print("<h1>Hello World</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
